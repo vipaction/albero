a0572177f40c03c2b61f297c0cbb1ae660715657
MAK$ <vipaction@ya.ru>
2015-01-27 21:54:22 +0200

    Add CSS stylesheet in /css/template.css and some icons in /images/ Edit template_view and main_view with css styles and div

--- code/views/main_view.php
+++ code/views/main_view.php
@@ -1,34 +1,34 @@
-<a href='/clients/search'>Новая задача</a>
-<br>
-<table cellspacing="0" border="1" bordercolor="grey" cellpadding="5">
-	<tr>
-		<th>№ заказа</th>
-		<th>Адрес</th>
-		<th>Телефон</th>
-		<th>Статус</th>
-	</tr>
-	<?php foreach ($data as $row): ?>
-		<tr>
-			<th>
-				<a href="/task/info/<?php  echo $row['rowid']; ?>">
-					<?php echo $row['rowid']; ?>
-				</a>
-			</th>
-			<td>
-				<?php echo $row['address']; ?>
-			</td>
-			<td>
-				<?php echo $row['phone']; ?>
-			</td>
-			<td>
-				<?php if ($row['name'] == 'close'): 
-					echo $row['value'];
-				else: ?>
-					<a href="/<?php  echo $row['name']; ?>/index/<?php  echo $row['rowid']; ?>">
-						<?php echo $row['value']; ?>
-					</a>
-				<?php endif ?>
-			</td>
+<div class="block_container">
+	<table cellpadding="5">
+		<tr align="left">
+			<th>№ заказа</th>
+			<th>Адрес</th>
+			<th>Телефон</th>
+			<th>Статус</th>
 		</tr>
-	<?php endforeach; ?>
-</table>
+		<?php foreach ($data as $row): ?>
+			<tr>
+				<th>
+					<a href="/task/info/<?php  echo $row['rowid']; ?>">
+						<?php echo $row['rowid']; ?>
+					</a>
+				</th>
+				<td>
+					<?php echo $row['address']; ?>
+				</td>
+				<td>
+					<?php echo $row['phone']; ?>
+				</td>
+				<td>
+					<?php if ($row['name'] == 'close'): 
+						echo $row['value'];
+					else: ?>
+						<a href="/<?php  echo $row['name']; ?>/index/<?php  echo $row['rowid']; ?>">
+							<?php echo $row['value']; ?>
+						</a>
+					<?php endif ?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+</div>
--- code/views/template_view.php
+++ code/views/template_view.php
@@ -4,16 +4,34 @@
 <head>
 	<meta charset="utf-8">
     <title>Albero di Vito</title>
+    <link rel="stylesheet" type="text/css" href="css/template.css">
 </head>
 <body>
-	<div>
+	<div class="main">
+		<div class="left_side">
+			<form method="post">
+				<button class="menu_icon" formaction="/main">
+					<img src="/images/tasks-icon.png">
+					<pre>Список задач</pre>
+				</button>
+				<button class="menu_icon" formaction="/clients">
+					<img src="/images/phonebook-icon.png">
+					<pre>Клиенты</pre>
+				</button>
+				<button class="menu_icon">
+					<img src="/images/calendar-icon.png">
+					<pre>Календарь</pre>
+				</button>
+			</form>
+		</div>
+		<div class="right_side">
+			<?php include ("code/views/".$content); ?>	
+		</div>
+	</div>
+	<!--<div>
 		<a href="/main">Список задач</a>
 		<a href='/clients'>Список клиентов</a>
-	</div>
-	<?php include ('code/views/'.$content); ?>	
-	<div>
-		<a href="/main">Список задач</a>
-		<a href='/clients'>Список клиентов</a>
-	</div>
+	</div>-->
+	
 </body>
 </html>
